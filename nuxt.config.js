a6bfff74298c04e6f375c323b54b13968d6f3422
Piphat <Piphat.u64@rsu.ac.th>
2024-09-05 15:40:38 +0700

    update title name

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - AQT Fix Port',
+    titleTemplate: '%s - AI Quant Tech Co.,Ltd',
     title: 'AQT Fix Port',
     htmlAttrs: {
       lang: 'en'
