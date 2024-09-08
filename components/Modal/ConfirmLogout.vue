76fa853ddc5f87da4ea9257cdde8578dce626a84
Piphat <Piphat.u64@rsu.ac.th>
2024-09-08 15:53:38 +0700

    Format Document

---
+++ components/Modal/ConfirmLogout.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-dialog persistent :retain-focus="false" v-model="open" max-width="400" max-height="300" content-class="rounded-xl"
+    @keydown.esc="cancel" @keydown.enter="confirm">
+    <v-card>
+      <v-card-title class="text-h5">
+        <v-icon justify="left" class="mr-3" size="30">mdi-logout-variant</v-icon>
+        DO YOU WANT TO LOGOUT?
+      </v-card-title>
+
+      <v-card-actions class="justify-center">
+        <v-btn color="primary" @click="confirm" class="rounded-xl font-weight-medium mt-0">
+          Logout
+        </v-btn>
+        <v-btn color="error" @click="cancel" class="rounded-xl font-weight-medium mt-0 ml-2">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    method: { type: Function },
+    open: {
+      required: true,
+    },
+  },
+  watch: {
+    open(val) {
+      if (val) {
+        document.addEventListener('keydown', this.handleKeydown);
+      } else {
+        document.removeEventListener('keydown', this.handleKeydown);
+      }
+    },
+  },
+  methods: {
+    confirm() {
+      this.method();
+      this.$emit("update:confirmLogout", false);
+    },
+    cancel() {
+      this.$emit("update:confirmLogout", false);
+    },
+    handleKeydown(e) {
+      if (e.key === 'Escape') {
+        this.cancel();
+      } else if (e.key === 'Enter') {
+        this.confirm();
+      }
+    },
+  },
+};
+</script>
