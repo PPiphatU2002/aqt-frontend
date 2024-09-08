b38a0e225eebc753ef90308f1ba709d04e552bb4
Piphat <Piphat.u64@rsu.ac.th>
2024-09-08 16:00:14 +0700

    Format Document

--- store/api/employee.js
+++ store/api/employee.js
@@ -1,42 +1,42 @@
 export const actions = {
-    async getEmployees({ getters }, data) {
-      return await this.$axios
+  async getEmployees({ getters }, data) {
+    return await this.$axios
       .get('employee', data)
       .then((res) => res.data)
-    },
-    async getEmployee({ getters }, no) {
-      return await this.$axios
+  },
+  async getEmployee({ getters }, no) {
+    return await this.$axios
       .get(`employee/${no}`)
       .then((res) => res.data)
-    },
-    async updateEmployee({ getters }, data) {
-     return await this.$axios
-     .$put(`employee/update-Employee/${data.no}`, data)
+  },
+  async updateEmployee({ getters }, data) {
+    return await this.$axios
+      .$put(`employee/update-Employee/${data.no}`, data)
       .then((res) => res.data)
-    },
-    async updateEmployeeAll({ getters }, data) {
-      return await this.$axios
+  },
+  async updateEmployeeAll({ getters }, data) {
+    return await this.$axios
       .put(`employee/update-employee-all/${data.no}`, data)
       .then((res) => res.data)
-    },
-    async deleteUser({ getters }, no) {
-      return await this.$axios
+  },
+  async deleteUser({ getters }, no) {
+    return await this.$axios
       .delete(`Employee/${no}`)
       .then((res) => res.data)
-    },
-    async updatePassword({ getters }, data) {
-      return await this.$axios
+  },
+  async updatePassword({ getters }, data) {
+    return await this.$axios
       .put(`employee/update-password/${data.no}`, data)
       .then((res) => res.data)
-    },
-    async register ({ getters }, data) {
-      return await this.$axios
+  },
+  async register({ getters }, data) {
+    return await this.$axios
       .post('auth/register', data)
       .then((res) => res.data)
-    },
-    async checkEmailDuplicate ({ getters }, data) {
-      return await this.$axios
+  },
+  async checkEmailDuplicate({ getters }, data) {
+    return await this.$axios
       .post(`auth/check-email-duplicate/`, data)
       .then((res) => res.data)
-    }
+  }
 }
--- store/api/log.js
+++ store/api/log.js
@@ -1,22 +1,22 @@
 export const actions = {
     async getLogs({ getters }, data) {
         return await this.$axios
-        .get('logs', data)
-        .then((res) => res.data)
+            .get('log', data)
+            .then((res) => res.data)
     },
     async getLogsID({ getters }, data) {
         return await this.$axios
-        .get('logs/:no', data)
-        .then((res) => res.data)
+            .get('log/:no', data)
+            .then((res) => res.data)
     },
     async getLogsType({ getters }, no) {
         return await this.$axios
-        .get(`logs/type/${no}`)
-        .then((res) => res.data)
+            .get(`log/type/${no}`)
+            .then((res) => res.data)
     },
     async addLogs({ getters }, data) {
         return await this.$axios
-        .post('logs', data)
-        .then((res) => res.data)
+            .post('log', data)
+            .then((res) => res.data)
     },
 }
