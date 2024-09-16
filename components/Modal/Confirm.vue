94473b42698409fb5ce51b275d3016d29f661659
Piphat <Piphat.u64@rsu.ac.th>
2024-09-16 16:22:06 +0700

    New Updated

--- components/Modal/Confirm.vue
+++ components/Modal/Confirm.vue
@@ -1,58 +1,57 @@
 <template>
-    <v-dialog persistent :retain-focus="false" v-model="open" max-width="200" max-height="300" content-class="rounded-xl"
-      @keydown.esc="cancel" @keydown.enter="confirm">
-      <v-card>
-        <v-card-title class="d-flex align-center justify-center text-h6">
-          <v-icon justify="center" class="mr-3" size="40" color="#24b224">mdi-account</v-icon>
-          ยืนยันหรือไม่?
-        </v-card-title>
-        <v-divider class="mb-3"></v-divider>
-  
-        <v-card-actions class="justify-center">
-          <v-btn color="#24b224" @click="confirm" class="rounded-xl font-weight-medium mt-0">
-            ตกลง
-          </v-btn>
-          <v-btn color="#e50211" @click="cancel" class="rounded-xl font-weight-medium mt-0">
-            ยกเลิก
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      method: { type: Function },
-      open: {
-        required: true,
-      },
+  <v-dialog persistent :retain-focus="false" v-model="open" max-width="200" max-height="300" content-class="rounded-xl"
+    @keydown.esc="cancel" @keydown.enter="confirm">
+    <v-card>
+      <v-card-title class="d-flex align-center justify-center text-h6">
+        <v-icon justify="center" class="mr-3" size="40" color="#24b224">mdi-account</v-icon>
+        ยืนยันหรือไม่?
+      </v-card-title>
+      <v-divider class="mb-3"></v-divider>
+
+      <v-card-actions class="justify-center">
+        <v-btn color="#24b224" @click="confirm" class="rounded-xl font-weight-medium mt-0">
+          ตกลง
+        </v-btn>
+        <v-btn color="#e50211" @click="cancel" class="rounded-xl font-weight-medium mt-0">
+          ยกเลิก
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    method: { type: Function },
+    open: {
+      required: true,
     },
-    watch: {
-      open(val) {
-        if (val) {
-          document.addEventListener('keydown', this.handleKeydown);
-        } else {
-          document.removeEventListener('keydown', this.handleKeydown);
-        }
-      },
+  },
+  watch: {
+    open(val) {
+      if (val) {
+        document.addEventListener('keydown', this.handleKeydown);
+      } else {
+        document.removeEventListener('keydown', this.handleKeydown);
+      }
     },
-    methods: {
-      confirm() {
-        this.method();
-        this.$emit("update:confirm", false);
-      },
-      cancel() {
-        this.$emit("update:confirm", false);
-      },
-      handleKeydown(e) {
-        if (e.key === 'Escape') {
-          this.cancel();
-        } else if (e.key === 'Enter') {
-          this.confirm();
-        }
-      },
+  },
+  methods: {
+    confirm() {
+      this.method();
+      this.$emit("update:confirm", false);
     },
-  };
-  </script>
-  
+    cancel() {
+      this.$emit("update:confirm", false);
+    },
+    handleKeydown(e) {
+      if (e.key === 'Escape') {
+        this.cancel();
+      } else if (e.key === 'Enter') {
+        this.confirm();
+      }
+    },
+  },
+};
+</script>
