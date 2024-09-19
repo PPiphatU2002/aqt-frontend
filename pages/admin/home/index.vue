4637ea97749cb8846a3fb090e45e30be56c6858a
Piphat <Piphat.u64@rsu.ac.th>
2024-09-19 17:55:30 +0700

    เพิ่มตำแหน่งใหม่ : แอดมิน

---
+++ pages/admin/home/index.vue
@@ -0,0 +1,99 @@
+<template>
+
+    <div>
+        <ModalWarning :open="modal.warning.open" :message="modal.warning.message" :warning.sync="modal.warning.open" />
+        <v-container class="fill-height" fluid justify="center" align="center">
+            <v-row justify="center">
+                <v-col cols="12" sm="8" md="4" :style="{ marginTop: '10vh' }">
+                    <v-row justify="center">
+                        <v-col cols="12" class="d-flex justify-center">
+                            <v-btn color="#24b224" @click="fixport"
+                                :style="{ fontSize: '20px', height: '60px', width: '150%', marginBottom: '15px' }">
+                                ข้อมูลหุ้นของลูกค้า
+                            </v-btn>
+                        </v-col>
+
+                        <v-col cols="12" class="d-flex justify-center">
+                            <v-btn color="#24b224" @click="transaction"
+                                :style="{ fontSize: '20px', height: '60px', width: '150%', marginBottom: '15px' }">
+                                ซื้อหุ้น
+                            </v-btn>
+                        </v-col>
+
+                        <v-col cols="12" class="d-flex justify-center">
+                            <v-btn color="#24b224" @click="management"
+                                :style="{ fontSize: '20px', height: '60px', width: '150%' }">
+                                ขายหุ้น
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+    
+</template>
+
+
+<script>
+
+export default {
+
+    layout: 'admin',
+    middleware: 'auth',
+
+    async mounted() {
+        await this.checkRank();
+    },
+
+    data() {
+        return {
+            modal: {
+                warning: {
+                    open: false,
+                    message: 'THE USER HAS NOT APPROVED IT!',
+                },
+            },
+        }
+    },
+
+    methods: {
+        async checkRank() {
+            if (this.$auth.loggedIn) {
+                const Status = this.$auth.user.status.toString();
+                const RankID = this.$auth.user.ranks_id.toString();
+                if (Status === '2') {
+                    this.$router.push('/');
+                    await this.$auth.logout();
+                }
+                else {
+                    if (RankID === '1') {
+                        this.$router.push('/developer/home');
+                    } else if (RankID === '2') {
+                        this.$router.push('/employee/home');
+                    } else if (RankID === '3') {
+                        this.$router.push('/admin/home');
+                    } else {
+                        this.$router.push('/auth');
+                    }
+                }
+            } else {
+                this.$router.push('/auth');
+            }
+        },
+
+        fixport() {
+            this.$router.push('/admin/transaction/customer_stock');
+        },
+
+        transaction() {
+            this.$router.push('/admin/transaction/buy');
+        },
+
+        management() {
+            this.$router.push('/admin/transaction/sale');
+        },
+    }
+}
+
+</script>
