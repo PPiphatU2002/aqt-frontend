048359299a5b366a7df96b02cb0f5b33de80d3c6
Piphat <Piphat.u64@rsu.ac.th>
2024-09-25 12:31:43 +0700

    เพิ่ม API ของ Stocks

---
+++ store/api/stock.js
@@ -0,0 +1,27 @@
+export const actions = {
+    async getStocks({ getters }, data) {
+        return await this.$axios
+            .get('stock', data)
+            .then((res) => res.data)
+    },
+    async getStock({ getters }, no) {
+        return await this.$axios
+            .get(`stock/${no}`)
+            .then((res) => res.data)
+    },
+    async addStock({ getters }, data) {
+        return await this.$axios
+            .post('stock', data)
+            .then((res) => res.data)
+    },
+    async updateStock({ getters }, data) {
+        return await this.$axios
+            .$put(`stock/update-stock/${data.no}`, data)
+            .then((res) => res.data)
+    },
+    async deleteStock({ getters }, no) {
+        return await this.$axios
+            .delete(`stock/${no}`)
+            .then((res) => res.data)
+    }
+}
